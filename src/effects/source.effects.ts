d42e0b123553d86b9a6083ac4186b2d19a5d6885
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-16 20:13:49 +0330

    Initial commit

---
+++ src/effects/source.effects.ts
@@ -0,0 +1,46 @@
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
+
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Action } from '@ngrx/store';
+import { Actions, Effect, toPayload } from '@ngrx/effects';
+import { of } from 'rxjs/observable/of';
+import { Store } from '@ngrx/store';
+
+import {
+  SourceActionTypes, SourceSubmit, SourceSubmitSucceed, SourceSubmitFailed
+} from '../actions';
+import { SourceModel, UpsertSourceApiModel } from 'app/models/source';
+import { SourceService } from '../services';
+
+@Injectable()
+export class SourceEffects {
+
+     constructor(
+         private actions$: Actions<any>,
+         private router: Router,
+         private sourceService: SourceService
+     ) { }
+
+     @Effect()
+     afterSubmitSource$ = this.actions$
+       .ofType(SourceActionTypes.SOURCE_SUBMIT)
+         .map(toPayload)
+       .switchMap((data: UpsertSourceApiModel.Request) => {
+         return this.sourceService.upsertSource(data)
+           .map(res => new SourceSubmitSucceed())
+           .catch(() => Observable.of(new SourceSubmitFailed()))
+       });
+
+     @Effect()
+     SigninSucceed$ = this.actions$
+       .ofType(SourceActionTypes.SOURCE_SUBMIT_SUCCEED)
+       .switchMap(() => {
+         this.router.navigate( ['source']);
+         return Observable.empty();
+       });
+
+}
