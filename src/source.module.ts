aa2afe05868a4a80b2fbc15c887347d80b242d7b
soushians noorghorbani <snoorghorbani@gmail.com>
2018-03-02 00:26:14 +0330

    2.0.0

--- src/source.module.ts
+++ src/source.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { StoreModule, Store } from "@ngrx/store";
-import { EffectsModule } from "@ngrx/effects";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { Observable } from "rxjs/Observable";
+import { NgModule } from '@angular/core';
+import { StoreModule, Store } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Observable } from 'rxjs/Observable';
 import {
 	MatIconModule,
 	MatButtonModule,
@@ -24,18 +24,18 @@ import {
 	MatToolbarModule,
 	MatDatepickerModule,
 	MatProgressBarModule
-} from "@angular/material";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { SourceReducers } from "./reducers";
+import { SourceReducers } from './reducers';
 //import {
 //        ConfigAppConfigComponent, AuthenticationModuleConfigComponent, UserModuleConfigComponent
 //} from './dumb-components';
-import { SourceService } from "./services";
-import { SourceListComponent, SourceUpsertComponent, SourceModuleContainerComponent } from "./smart-components";
-import { RoutingModule } from "./source.routing-module";
-import { SourceEffects } from "./effects";
+import { SourceService } from './services';
+import { SourceListComponent, SourceUpsertComponent, SourceModuleContainerComponent } from './smart-components';
+import { RoutingModule } from './source.routing-module';
+import { SourceEffects } from './effects';
 // import { DiagramModule } from "app/diagram";
 //import { ConfigLoaded } from 'app/config/actions';
 
@@ -64,7 +64,7 @@ import { SourceEffects } from "./effects";
 		MatDatepickerModule,
 		MatProgressBarModule,
 		BrowserModule,
-		// StoreModule.forFeature('source', SourceReducers),
+		StoreModule.forFeature('source', SourceReducers),
 		BrowserAnimationsModule,
 		RoutingModule,
 		// DiagramModule,
